2240da3dea2a196d576cb298cc6cd4914457fabb
Futo Sawaguchi <kaede.tyu3@icloud.com>
2026-04-21 22:40:23 +0900

    chore: adjust SEND_INTERVAL to 0.4

--- config.py
+++ config.py
@@ -26,7 +26,3 @@ MIN_ANGLE_CHANGE = 5.0
 # 首の可動範囲
 HEAD_Y_MAX = 300.0   # 左右
 HEAD_P_MAX = 100.0   # 前後
-
-# 顔消失時のキョロキョロ設定
-SEARCH_ANGLES  = [-200, 200, -200, 0]  # キョロキョロの角度列
-SEARCH_INTERVAL = 0.8                  # 各角度の待機時間（秒）
